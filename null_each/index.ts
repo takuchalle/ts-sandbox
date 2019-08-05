7d4f5d0083f59f0ad3896963051fa4418e72f41d
Takuya OHASHI <tky.ohashi@gmail.com>
2019-08-05 14:01:29 +0900

    Add each

---
+++ null_each/index.ts
@@ -0,0 +1,12 @@
+//const members: string[] = null;
+
+var members = ["ushio", "kasuga", "wakabayashi"];
+
+if (members != null) {
+    for (const member of members) {
+        console.log(member);
+    }
+}
+
+members.forEach((member) => console.log(member));
+
